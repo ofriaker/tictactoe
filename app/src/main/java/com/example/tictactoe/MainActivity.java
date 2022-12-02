50103366861afe1215a06c96f1991c2784b67678
ofriaker <ofriaker@gmail.com>
2022-12-02 22:55:16 +0200

    fix: all buttons work and in place, added which turn it is

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,12 +14,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int turnCount = 0;
     boolean isVictory = false;
     ImageButton btnImages [][] = new ImageButton[3][3];
-
+    ImageView xPlay;
+    ImageView oPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        xPlay = findViewById(R.id.xPlay);
+        oPlay = findViewById(R.id.oPlay);
+        oPlay.setVisibility(View.INVISIBLE);
 
         for(int i=0; i<3; i++) {
             for(int j=0; j<3; j++) {
@@ -33,15 +38,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        view.setVisibility(View.VISIBLE);
         if(turnCount < 9 && !isVictory) {
             if (Xturn) {
                 view.setBackgroundResource(R.drawable.x);
                 Xturn = false;
+                xPlay.setVisibility(View.INVISIBLE);
+                oPlay.setVisibility(View.VISIBLE);
             }
             else {
                 view.setBackgroundResource(R.drawable.o);
                 Xturn = true;
+                xPlay.setVisibility(View.VISIBLE);
+                oPlay.setVisibility(View.INVISIBLE);
             }
             turnCount++;
         }
