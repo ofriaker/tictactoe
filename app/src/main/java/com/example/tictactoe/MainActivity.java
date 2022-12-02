9901ba59ce7ecf415c083ecf1c7b96d3e97f4b7d
ofriaker <ofriaker@gmail.com>
2022-12-02 22:34:25 +0200

    fix; all buttons work now and change images

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,12 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     boolean Xturn = true;
     int turnCount = 0;
     boolean isVictory = false;
     ImageButton btnImages [][] = new ImageButton[3][3];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +25,25 @@ public class MainActivity extends AppCompatActivity {
                 String btnImgId = "img" +i + j;
                 int resID = this.getResources().getIdentifier(btnImgId, "id", this.getPackageName());
                 ImageButton btn = findViewById(resID);
-                btn.setOnClickListener(view -> {
-                    if(turnCount < 9 && !isVictory) {
-                        if (Xturn) {
-                            view.setBackgroundResource(R.drawable.x);
-                            Xturn = false;
-                        }
-                        else view.setBackgroundResource(R.drawable.o);
-                        turnCount++;
-                    }
-                });
+                btn.setOnClickListener(this);
                 btnImages[i][j] = btn;
             }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        view.setVisibility(View.VISIBLE);
+        if(turnCount < 9 && !isVictory) {
+            if (Xturn) {
+                view.setBackgroundResource(R.drawable.x);
+                Xturn = false;
+            }
+            else {
+                view.setBackgroundResource(R.drawable.o);
+                Xturn = true;
+            }
+            turnCount++;
         }
     }
 
