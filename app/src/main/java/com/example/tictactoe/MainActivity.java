b0176aee2c77886507ede484c8c60e48c2020b8c
ofriaker <ofriaker@gmail.com>
2022-12-02 18:50:11 +0200

    add on click method - doesnt work now

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,39 +2,41 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+
+    boolean Xturn = true;
+    int turnCount = 0;
+    boolean isVictory = false;
+    ImageButton btnImages [][] = new ImageButton[3][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton btnImages [][] = new ImageButton[3][3];
-//        img00.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//               v.setBackgroundResource(R.drawable.x);
-//
-//            }
-//        });
-
         for(int i=0; i<3; i++) {
             for(int j=0; j<3; j++) {
                 String btnImgId = "img" +i + j;
-                int resID = getResources().getIdentifier(btnImgId, "id", getPackageName());
-                btnImages[i][j] = findViewById(resID);
-                btnImages[i][j].setOnClickListener((View.OnClickListener) this);
+                int resID = this.getResources().getIdentifier(btnImgId, "id", this.getPackageName());
+                ImageButton btn = findViewById(resID);
+                btn.setOnClickListener(view -> {
+                    if(turnCount < 9 && !isVictory) {
+                        if (Xturn) {
+                            view.setBackgroundResource(R.drawable.x);
+                            Xturn = false;
+                        }
+                        else view.setBackgroundResource(R.drawable.o);
+                        turnCount++;
+                    }
+                });
+                btnImages[i][j] = btn;
             }
         }
     }
 
-    @Override
-    public void onClick(View view) {
-
-    }
 }
